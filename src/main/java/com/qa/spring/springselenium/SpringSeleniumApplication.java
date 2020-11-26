ff34c8a7775bbc82efca961e8f57acd2cf78f52b
satyar3 <satyaranjan0401@gmail.com>
2020-11-26 11:06:33 +0530

    Comment added for step defs

--- src/main/java/com/qa/spring/springselenium/SpringSeleniumApplication.java
+++ src/main/java/com/qa/spring/springselenium/SpringSeleniumApplication.java
@@ -3,6 +3,7 @@ package com.qa.spring.springselenium;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Add this annotation for all step def files
 @SpringBootApplication
 public class SpringSeleniumApplication {
 

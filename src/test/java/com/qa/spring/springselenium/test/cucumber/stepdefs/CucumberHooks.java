8a95c29bede1247f295db8205aa616e4cdf05e03
satyar3 <satyaranjan0401@gmail.com>
2020-12-12 16:51:00 +0530

    Added browser quit

--- src/test/java/com/qa/spring/springselenium/test/cucumber/stepdefs/CucumberHooks.java
+++ src/test/java/com/qa/spring/springselenium/test/cucumber/stepdefs/CucumberHooks.java
@@ -1,13 +1,19 @@
 package com.qa.spring.springselenium.test.cucumber.stepdefs;
 
 import com.qa.spring.springselenium.service.ScreenshotService;
+import io.cucumber.java.After;
 import io.cucumber.java.AfterStep;
 import io.cucumber.java.Scenario;
+import org.openqa.selenium.WebDriver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Lazy;
 
 public class CucumberHooks {
+
+    @Autowired
+    private ApplicationContext applicationContext;
 
     @Lazy
     @Autowired
@@ -17,5 +23,10 @@ public class CucumberHooks {
     public void afterStep(Scenario scenario){
         if(scenario.isFailed())
             scenario.embed(screenshotService.getScreenShot(), "image/png", scenario.getName());
+    }
+
+    @After
+    public void quitBrowser(){
+        applicationContext.getBean(WebDriver.class).quit();
     }
 }
